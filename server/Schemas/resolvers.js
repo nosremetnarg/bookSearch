5f0120a6ae3c46f34091ba70bc6582db736f130c
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-23 14:48:17 -0500

    working through resolvers and queries

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,27 @@
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        
+    }
+}
+
+
+
+module.exports = resolvers;
---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,60 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook([Author]): User
+    removeBook(bookId: ID!): User
+}
+
+type Author {
+    _id: ID
+    description: String
+    bookId: String
+    image: Image
+    links: String
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Book { 
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: string
+
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
